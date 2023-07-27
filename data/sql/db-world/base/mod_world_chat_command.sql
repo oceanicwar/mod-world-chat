49d328139607cc38556f3a5d33a0fd33e9734cd6
jshplayer <jshplayer@outlook.com>
2023-07-27 20:26:09 +1200

    Move to Prod

--- data/sql/db-world/base/mod_world_chat_command.sql
+++ data/sql/db-world/base/mod_world_chat_command.sql
@@ -1,6 +1,6 @@
-DELETE FROM `command` WHERE `name` IN ('chat', 'chata', 'chath');
+DELETE FROM `command` WHERE `name` IN ('world', 'worlda', 'worldh');
 
 INSERT INTO `command` (`name`, `security`, `help`) VALUES 
-('chata', 1, 'Syntax: .chata $text - To speak as a GM only to Alliance'),
-('chath', 1, 'Syntax: .chath $text - To speak as a GM only to Horde'),
-('chat', 0, 'Syntax: .chat $text\n.chat on To show World Chat\n.chat off To hide World Chat');
+('worlda', 1, 'Syntax: .worlda - Speak as a GM only to Alliance'),
+('worldh', 1, 'Syntax: .worldh - Speak as a GM only to Horde'),
+('world', 0, 'Syntax: .world - Enter anything after this command to speak to all online players.');
